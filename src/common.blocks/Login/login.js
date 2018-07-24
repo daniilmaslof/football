451450b05be4aa6d5662fa1b924461c7527c16e0
Daniil Maslov <daniil.maslov@saritasa.com>
2018-07-24 10:11:48 +0500

    add component login

--- src/common.blocks/Login/login.js
+++ src/common.blocks/Login/login.js
@@ -1,13 +1,46 @@
+const loader = new Loader();
+const DictMessagesByErrorCode = new Map();
+
+DictMessagesByErrorCode.set(503, 'please repeat request');
+DictMessagesByErrorCode.set(404, 'Incorrect login or password');
+DictMessagesByErrorCode.set(20, 'Please wait response load');
+
 /**
- * Run after load pages and  loginForm addEventListener submit = function submitForm.
+ * Create dom span with error.
+ *
+ * @param {Object} error  Error occurred while sending.
  */
-function onLoad() {
+function createError(error) {
+  const previousErrorElement = document.getElementById(`error`);
+
+  const errorMessage = `error: \n\r ${DictMessagesByErrorCode.get(error.code)}`;
+
+  if (previousErrorElement && previousErrorElement.innerText !== errorMessage) {
+    previousErrorElement.remove();
+  }
+
+  const currentErrorElement = document.createElement('span');
+
+  currentErrorElement.classList.add('error');
+  currentErrorElement.id = 'error';
+  currentErrorElement.appendChild(
+    document.createTextNode(`error: \n\r ${DictMessagesByErrorCode.get(error.code)}`),
+  );
+
   const form = document.getElementById('loginForm');
 
-  form.addEventListener('submit', submitForm);
+  form.appendChild(currentErrorElement);
 }
 
-const loader = new Loader();
+/**
+ * Store token in local storage and redirect Table .
+ *
+ * @param {Object} dataResponse Coming from the server and  containing token.
+ */
+function login(dataResponse) {
+  window.localStorage.setItem('token', dataResponse.token);
+  window.location.href = '../table/table.html';
+}
 
 /**
  * Post email and password and create callback login with a positive response and createError with a negative.
@@ -17,10 +50,9 @@ const loader = new Loader();
 function submitForm(event) {
   event.preventDefault();
 
-  const form = document.getElementById('loginForm');
+  const email = document.getElementById('login').value;
 
-  const email = form.getElementById('login').value;
-  const password = form.getElementById('password').value;
+  const password = document.getElementById('password').value;
 
   const data = {
     email,
@@ -34,35 +66,11 @@ function submitForm(event) {
   );
 }
 
-const DictMessagesByErrorCode = new Map();
-
-DictMessagesByErrorCode.set(503, 'please repeat request');
-DictMessagesByErrorCode.set(404, 'Incorrect login or password');
-DictMessagesByErrorCode.set(20, 'Please wait response load');
-
 /**
- * Create dom span with error.
- *
- * @param {Object} error  Error occurred while sending.
+ * Run after load pages and  loginForm addEventListener submit = function submitForm.
  */
-function createError(error) {
-  const errorElement = document.createElement('span');
-
-  errorElement.appendChild(
-    document.createTextNode(`error: \n\r ${DictMessagesByErrorCode.get(error.code)}`),
-  );
-
+function onLoad() {
   const form = document.getElementById('loginForm');
 
-  form.appendChild(errorElement);
-}
-
-/**
- * Store token in local storage and redirect Table .
- *
- * @param {Object} dataResponse Coming from the server and  containing token.
- */
-function login(dataResponse) {
-  window.localStorage.setItem('token', dataResponse.token);
-  window.location.href = 'src/common.blocks/form/form.html';
+  form.addEventListener('submit', submitForm);
 }
