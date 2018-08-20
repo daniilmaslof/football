033d6333bb2f3ed4e0a27d77cd4d39ddd02a6729
Daniil Maslov <daniil.maslov@saritasa.com>
2018-08-20 13:15:13 +0500

    Add getting the roles.

--- carsTable/src/app/core/services/login.service.ts
+++ carsTable/src/app/core/services/login.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient, HttpRequest } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NgxPermissionsService } from 'ngx-permissions';
 import { Observable } from 'rxjs/Observable';
-import { tap } from 'rxjs/operators';
+import { map, take, tap } from 'rxjs/operators';
 
 /**
  * Service communication with http server https://backend-jscamp.saritasa-hosting.com/api/auth.
@@ -13,19 +14,42 @@ export class LoginService {
 
   /**
    * @param httpClient standard angular http communication class.
+   * @param permissionService that allows you to manipulate permission.
    */
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private permissionService: NgxPermissionsService) {
   }
 
   /**
-   * Send password and email and if they are correct save token in Local Storage.
+   * Send password and email and if they are correct save token in Local Storage and load a client role.
    *
    * @param email entered by the user.
    * @param password entered by the user.
    */
   public login(email: string, password: string): Observable<any> {
     return this.httpClient.post('https://backend-jscamp.saritasa-hosting.com/api/auth', { email, password }).pipe(
-      tap(response => localStorage.setItem('token', response['token'])),
+      tap(response => this.setSession(response.token)),
     );
+  }
+
+  /**
+   * Save token in Local Storage and load a client role.
+   *
+   * @param token authentication.
+   */
+  private setSession(token: string): void {
+    localStorage.setItem('token', token);
+    this.getRole().then(
+      role => this.permissionService.addPermission(role),
+      () => this.permissionService.addPermission('GUEST'),
+    );
+  }
+  /**
+   * Get the  logged client role.
+   */
+  public getRole(): Promise<any> {
+    return this.httpClient.get('https://backend-jscamp.saritasa-hosting.com/api/profile').pipe(
+      map((role: any) => role.role.name.toUpperCase()),
+      take(1),
+    ).toPromise();
   }
 }
